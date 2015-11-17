819d582d424a3340808083717278d4bbd3bb2790
40323225 <40323225@gm.nfu.edu.tw>
2015-11-17 15:53:01 +0800

    1117

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -8,7 +8,7 @@ from __future__ import unicode_literals
 import os
 
 AUTHOR = 'KMOL'
-SITENAME = '2015FALL KMOL 課程'
+SITENAME = '2015FALL cadp 課程'
 SITEURL = ''
 
 PATH = 'content'
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),
+         ('Python', 'http://python.org/'),('40323225', 'http://2015fallhw.github.io/2015fallcadpb/user/40323225/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
